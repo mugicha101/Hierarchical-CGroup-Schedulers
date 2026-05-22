72573fcc66889e21b2c7b928210f8ba71faabc82
Alexander Yoshida <alexander.yoshida@gmail.com>
2026-05-21 23:17:42 -0400

    add missing chown to perm_setup script

--- perm_setup.sh
+++ perm_setup.sh
@@ -6,6 +6,6 @@ if [ -z "$SUDO_USER" ]; then
   echo "This script must be run with sudo."
   exit 1
 fi
-chown $SUDO_USER:$SUDO_USER /sys/fs/cgroup /sys/fs/bpf
+chown $SUDO_USER:$SUDO_USER /sys/fs/cgroup /sys/fs/bpf /sys/fs/cgroup/cgroup.subtree_control /sys/fs/cgroup/cgroup.procs /sys/fs/cgroup/cgroup.threads
 chmod u+rwx /sys/fs/cgroup /sys/fs/bpf
 chmod u+rw /sys/fs/cgroup/cgroup.subtree_control /sys/fs/cgroup/cgroup.procs /sys/fs/cgroup/cgroup.threads
